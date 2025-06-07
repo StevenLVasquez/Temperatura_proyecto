ffa43971fd33278871ee8f57a66359f703c9d65f
MichaelEGS <mz2022-2135@uce.edu.do>
2025-06-07 14:43:26 -0400

    TXTPROMEDIO

--- Temperatura_proyecto/Form1.Designer.cs
+++ Temperatura_proyecto/Form1.Designer.cs
@@ -28,25 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.ZoneNum = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
-            this.button1 = new System.Windows.Forms.Button();
+            this.ZoneNum2 = new System.Windows.Forms.NumericUpDown();
+            this.BtnMostrar = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoneNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoneNum2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // numericUpDown1
+            // ZoneNum
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(29, 130);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 0;
+            this.ZoneNum.Location = new System.Drawing.Point(29, 130);
+            this.ZoneNum.Name = "ZoneNum";
+            this.ZoneNum.Size = new System.Drawing.Size(120, 20);
+            this.ZoneNum.TabIndex = 0;
             // 
             // label1
             // 
@@ -54,7 +57,7 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(26, 82);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(187, 16);
+            this.label1.Size = new System.Drawing.Size(182, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Cuantas zonas desea ver";
             // 
@@ -74,25 +77,25 @@
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(26, 190);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(318, 16);
+            this.label4.Size = new System.Drawing.Size(309, 16);
             this.label4.TabIndex = 4;
             this.label4.Text = "Cuantos sensores quieres ver en esta zona:";
             // 
-            // numericUpDown2
+            // ZoneNum2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(29, 237);
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown2.TabIndex = 5;
+            this.ZoneNum2.Location = new System.Drawing.Point(29, 237);
+            this.ZoneNum2.Name = "ZoneNum2";
+            this.ZoneNum2.Size = new System.Drawing.Size(120, 20);
+            this.ZoneNum2.TabIndex = 5;
             // 
-            // button1
+            // BtnMostrar
             // 
-            this.button1.Location = new System.Drawing.Point(495, 163);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Mostrar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BtnMostrar.Location = new System.Drawing.Point(495, 163);
+            this.BtnMostrar.Name = "BtnMostrar";
+            this.BtnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.BtnMostrar.TabIndex = 6;
+            this.BtnMostrar.Text = "Mostrar";
+            this.BtnMostrar.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
@@ -104,10 +107,28 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtPromedio);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1058, 309);
             this.panel1.TabIndex = 8;
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(758, 117);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(159, 20);
+            this.txtPromedio.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(767, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Promedio de temperatura";
             // 
             // Form1
             // 
@@ -115,18 +136,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1231, 626);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.BtnMostrar);
+            this.Controls.Add(this.ZoneNum2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.ZoneNum);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoneNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoneNum2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,14 +157,16 @@
 
         #endregion
 
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown ZoneNum;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.NumericUpDown numericUpDown2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown ZoneNum2;
+        private System.Windows.Forms.Button BtnMostrar;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.Label label3;
     }
 }
 
